6c0d15bdd2383f0cca352e3d118c747a02c6e6ed
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2016-12-29 21:16:05 -0200

    Allow robot to move one unit forward.

--- lib/toy_robot/robot.rb
+++ lib/toy_robot/robot.rb
@@ -9,5 +9,17 @@ module ToyRobot
     def report
       "#{@x},#{@y},#{@facing.to_s.upcase}"
     end
+
+    def move
+      if @facing == :north
+        @y += 1
+      elsif @facing == :east
+        @x += 1
+      elsif @facing == :south
+        @y -= 1
+      elsif @facing == :west
+        @x -= 1
+      end
+    end
   end
 end
--- spec/toy_robot/robot_spec.rb
+++ spec/toy_robot/robot_spec.rb
@@ -8,4 +8,32 @@ RSpec.describe ToyRobot::Robot do
       expect(robot.report).to eq "0,0,SOUTH"
     end
   end
+
+  describe "#move" do
+    it "moves robot one unit when facing north" do
+      robot.place 0, 0, :north
+      robot.move
+
+      expect(robot.report).to eq "0,1,NORTH"
+    end
+
+    it "moves robot one unit when facing east" do
+      robot.place 0, 0, :east
+      robot.move
+
+      expect(robot.report).to eq "1,0,EAST"
+    end
+
+    it "moves robot one unit when facing south" do
+      robot.place 0, 4, :south
+      robot.move
+      expect(robot.report).to eq "0,3,SOUTH"
+    end
+
+    it "moves robot one unit when facing west" do
+      robot.place 4, 4, :west
+      robot.move
+      expect(robot.report).to eq "3,4,WEST"
+    end
+  end
 end
