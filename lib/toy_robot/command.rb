b38c7df9ac48b84e071ce3762872ed01853e355f
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2017-03-09 23:00:48 +0100

    Include new commands on “load path”.

--- lib/toy_robot/command.rb
+++ lib/toy_robot/command.rb
@@ -1,5 +1,8 @@
 require "toy_robot/command/place.rb"
 require "toy_robot/command/report.rb"
+require "toy_robot/command/move.rb"
+require "toy_robot/command/left.rb"
+require "toy_robot/command/right.rb"
 require "toy_robot/command/quit.rb"
 require "toy_robot/command/registry.rb"
 
