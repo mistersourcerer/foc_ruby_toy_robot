b4cfc5e11c22ba7ca4aca314054f8be54841891e
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2017-03-09 20:01:49 +0100

    Use `Register` on the CLI.

--- lib/toy_robot/cli.rb
+++ lib/toy_robot/cli.rb
@@ -1,10 +1,18 @@
 module ToyRobot
   class CLI
+    def initialize
+      @robot = ToyRobot::Robot.new
+    end
+
     def run
       while command = gets
         if command.chomp.upcase == "QUIT"
           exit(0)
         end
+
+        register = Command::Register.new
+        command = register.from command
+        command.perform @robot
       end
     end
   end
