bb56993e77f36141100aba8f236888f00b25fa0e
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2017-03-09 20:01:26 +0100

    Add the place command.

---
+++ spec/toy_robot/command/place_spec.rb
@@ -0,0 +1,21 @@
+require "spec_helper"
+
+describe Command::Place do
+  let(:robot) { ToyRobot::Robot.new }
+  let(:command) { described_class.new }
+
+  describe "#perform" do
+    it "knows how to place a robot" do
+      command.configure "2,2,NORTH"
+      command.perform robot
+
+      expect(robot.report).to eq "2,2,NORTH"
+    end
+  end
+
+  describe "#recognize?" do
+    it "returns true for name 'PLACE'" do
+      expect(command.recognize? "PLACE").to eq true
+    end
+  end
+end
